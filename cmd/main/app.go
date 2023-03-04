34451e8cf51bfef0e9417a4645d65621d4215114
jumaniyozov <jumaniyozovse@gmail.com>
2023-03-04 16:04:37 +0500

    logger setup

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -4,23 +4,27 @@ import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"github.com/jumaniyozov/grest/internal/user"
-	"log"
+	"github.com/jumaniyozov/grest/pkg/logging"
 	"net"
 	"net/http"
 	"time"
 )
 
 func main() {
-	log.Println("Initializing router")
+	logger := logging.GetLogger()
+	logger.Info("Creating router")
 	router := httprouter.New()
 
-	userHandler := user.NewHandler()
+	userHandler := user.NewHandler(logger)
 	userHandler.Register(router)
 
 	start(router)
 }
 
 func start(router *httprouter.Router) {
+	logger := logging.GetLogger()
+	logger.Info("Starting server...")
+
 	port := "8080"
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
 	if err != nil {
@@ -33,6 +37,6 @@ func start(router *httprouter.Router) {
 		ReadTimeout:  15 * time.Second,
 	}
 
-	log.Println("Starting application on port: ", port)
-	log.Fatalln(server.Serve(listener))
+	logger.Info("Starting application on port: ", port)
+	logger.Fatal(server.Serve(listener))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/jumaniyozov/grest
 
 go 1.20
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/rs/zerolog v1.29.0 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+)
